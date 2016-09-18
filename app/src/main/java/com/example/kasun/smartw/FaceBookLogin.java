2c2b0578cde45d195525179f89199c87f8d92e81
Kasun <kasunkarunarathna@hotmail.com>
2016-09-18 23:00:35 +0530

    Test Commit

--- app/src/main/java/com/example/kasun/smartw/FaceBookLogin.java
+++ app/src/main/java/com/example/kasun/smartw/FaceBookLogin.java
@@ -44,7 +44,7 @@ public class FaceBookLogin extends AppCompatActivity implements LoaderCallbacks<
      */
     private static final int REQUEST_READ_CONTACTS = 0;
 
-    //test Module
+    //test Modulee
     private static int cnCOunt=10;
     /**
      * A dummy authentication store containing known user names and passwords.
