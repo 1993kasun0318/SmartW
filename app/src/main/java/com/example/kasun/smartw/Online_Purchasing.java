65eaebf7bf2b82d8ba9d17319b3a973cfc18ffa8
Kasun <kasunkarunarathna@hotmail.com>
2016-10-01 13:05:17 +0530

    Main Layout Applied

--- app/src/main/java/com/example/kasun/smartw/Online_Purchasing.java
+++ app/src/main/java/com/example/kasun/smartw/Online_Purchasing.java
@@ -3,8 +3,9 @@ package com.example.kasun.smartw;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,15 +13,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
-public class SmartWardrobe extends AppCompatActivity
+public class Online_Purchasing extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_smart_wardrobe);
+        setContentView(R.layout.activity_online__purchasing);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -35,12 +35,11 @@ public class SmartWardrobe extends AppCompatActivity
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+            this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-
         navigationView.setNavigationItemSelectedListener(this);
     }
 
@@ -57,7 +56,7 @@ public class SmartWardrobe extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.smart_wardrobe, menu);
+        getMenuInflater().inflate(R.menu.online__purchasing, menu);
         return true;
     }
 
@@ -69,14 +68,13 @@ public class SmartWardrobe extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            //Settings Intent
+        if (id == R.id.action_settingsPur) {
             Intent settingsIntent=new Intent(this,SmartW_Settings.class);
             startActivity(settingsIntent);
-            return true;
         }
-        if (id == R.id.action_logout) {
-            return true;
+        if(id==R.id.action_logoutPur)
+        {
+
         }
 
         return super.onOptionsItemSelected(item);
@@ -88,19 +86,14 @@ public class SmartWardrobe extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.smart_wardrobeMenu) {
+        if (id == R.id.nav_camera) {
             // Handle the camera action
-        } else if (id == R.id.style_networkMenu) {
-            //Open Style Network Activity When Menu Item Selected
-            Intent styleNetworkIntent=new Intent(this,Style_Network.class);
-            startActivity(styleNetworkIntent);
-        } else if (id == R.id.online_ShopMenu) {
-            //Open Online Purchasing Activity When Menu Item Selected
-            Intent onlinePurchasingIntent=new Intent(this,Online_Purchasing.class);
-            startActivity(onlinePurchasingIntent);
-        } else if (id == R.id.settingsMenu) {
-            Intent settingsIntent=new Intent(this,SmartW_Settings.class);
-            startActivity(settingsIntent);
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
